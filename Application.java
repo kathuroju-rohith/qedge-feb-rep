959c60cc17d46d527dcc4c3997a01970fce9af20
kathuroju-rohith <kathuroju_rohith@yahoo.com>
2025-02-24 16:55:53 +0530

    Java files added

--- Application.java
+++ Application.java
@@ -2,7 +2,29 @@ import java.util.Scanner;
 
 public class HelloWorld {
 
+    ## Added feature 201.V1
+	
+	
+	import java.util.Scanner;
+
+public class HelloWorld {
+
     public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
+}
+	
+	public static void main(String[] args) {
 
         // Creates a reader instance which takes
         // input from standard input - keyboard
